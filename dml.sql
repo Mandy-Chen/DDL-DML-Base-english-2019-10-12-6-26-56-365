9530884dbda0ea05427ff295d627ca77d3a52ac5
Mandy-Chen <57031811+Mandy-Chen@users.noreply.github.com>
2020-06-27 15:59:43 +0800

    Update dml.sql
    answer

--- dml.sql
+++ dml.sql
@@ -1,7 +1,16 @@
 -- Insert record
+insert into student 
+(id, name,age,sex)
+values
+( 003, "Mandy",23,"female");
 
 -- Revise record
+update student
+set age=18
+where id=003;
 
 --  Delete record
+delete from student where id=003;
 
 -- Search record
+select * from student where id=001;
